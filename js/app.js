a7eda425f9c6a44ec84f9bb0b933d2d1e4d8d3c7
Nycole <nycwalsh@gmail.com>
2019-01-08 12:34:46 -0600

    Updating fetch

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
+const userArray = [];
+const container = document.getElementById('container');
+
+// REUSABLE FETCH FUNCTION
 function goFetch(url) {
   return fetch(url)
+    .then(checkOK)
     .then(response => response.json)
+    .catch(new Error('Fetch failed.'))
 };
 
-goFetch('https://randomuser.me/api/?results=12');
+// FETCH RANDOM USERS
+goFetch('https://randomuser.me/api/?results=12&nat=us,gb')
+  .then(info => userArray.push(info))
+  .then(loop => {
+    for (let i = 0, i <= userArray[0].results.length, i+=1) {
+      const users = userArray[0].results;
+      const createLink = document.createElement('a');
+      container.appendChild('createLink');
+
+      let populateCard = `
+        <div class="card">
+          <div class="pic">
+            <img src="${users[i].picture.medium}" alt="Photo of ${users[i].name.first} ${users[i].name.last}">
+          </div>
+          <h2>${users[i].name.first} ${users.name.last}</h2>
+          <a href="mailto:${users[i].email}">${users[i].email}</a>
+          <p>${users[i].location.city}</p>
+      `;
+    }
+  })
